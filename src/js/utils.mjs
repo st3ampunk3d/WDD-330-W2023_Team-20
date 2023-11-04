b6ad48762c3205bbc9488dd5c5260b1887e9c4c6
syuyrw <jayreitz@gmail.com>
2023-11-04 14:06:02 -0600

    My work

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -9,23 +9,32 @@ export function qs(selector, parent = document) {
 export function getLocalStorage(key) {
   var cartArray = JSON.parse(localStorage.getItem(key) || "[]");
   if (cartArray == null) {
-    cartArray = []
+    cartArray = [];
   }
   return cartArray;
 }
 // save data to local storage
 export function setLocalStorage(key, data, action = "add") {
-  var cartArray = JSON.parse(
-    localStorage.getItem(key) || "[]"
-  );
+  try {
+    let cartArray = JSON.parse(localStorage.getItem(key) || "[]");
 
-  if (action == "add") {
-    cartArray.push(data)
-  } else {
-    const index = cartArray.map(item => item.Id).indexOf(data)
-    cartArray.splice(index, 1)
+    if (!Array.isArray(cartArray)) {
+      cartArray = []; // Initialize as an empty array if it's not an array
+    }
+
+    if (action === "add") {
+      cartArray.push(data);
+    } else {
+      const index = cartArray.findIndex((item) => item.Id === data.Id);
+      if (index !== -1) {
+        cartArray.splice(index, 1);
+      }
+    }
+
+    localStorage.setItem(key, JSON.stringify(cartArray));
+  } catch (error) {
+    console.error(error);
   }
-  localStorage.setItem(key, JSON.stringify(cartArray));
 }
 
 // set a listener for both touchend and click
