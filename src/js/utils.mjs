df14521976ea1700920e42794a4c10e5bb9fe621
mr.beale <j.b.beale2015@gmail.com>
2023-11-06 22:17:11 -0700

    Created Dynamic Details page and produc list

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -43,3 +43,26 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get('product')
+
+  return product
+}
+
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
