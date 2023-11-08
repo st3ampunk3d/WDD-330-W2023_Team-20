64164c4bd78218b51ba77e00da1522e35b7623f2
mr.beale <j.b.beale2015@gmail.com>
2023-11-07 21:54:13 -0700

    formatting. Changed vite conf for dynamic products

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,9 +2,10 @@ import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  console.log(cartItems)
-  const [quantities, uniqueValues] = getItemCount(cartItems)
-  const htmlItems = uniqueValues.map((item) => cartItemTemplate(item, quantities));
+  const [quantities, uniqueItems] = itemQuantity(cartItems);
+  const htmlItems = uniqueItems.map((item) =>
+    cartItemTemplate(item, quantities)
+  );
   let htmlTotal = `Total: $${cartTotal(cartItems)}`;
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
   document.querySelector(".cartTotal").innerHTML = htmlTotal;
@@ -22,24 +23,22 @@ function cartItemTemplate(item, quantites) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__quantity">qty: ${quantites.get(item.Id)}</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
-  <span class="removeFromCart" data-id="${item.Id}">X<span>
-  <span class="quantity">${quantites.get(item.Id)}</span>
+  <span><span class="removeFromCart" data-id="${item.Id}">X</span>
+  <span class="decrement" data-id="${item.Id}"> - </span>
+  <span class="addToCart" data-id="${item.Id}"> + </span></span>
 </li>`;
 
   return newItem;
 }
 
-function cartTotal(item){
-
+function cartTotal(item) {
   let price = 0;
   item.map((items) => {
     price = price + items.FinalPrice;
-
-  })  
+  });
   return price;
-
 }
 
 renderCartContents();
@@ -59,26 +58,40 @@ allCartItems.forEach(function (item) {
   item.addEventListener("click", removeFromCartHandler);
 });
 
+function decrementCart(product) {
+  setLocalStorage("so-cart", product, "remove");
+}
 
-function getItemCount(data) {
-  var jsonArray = data
-  var map = new Map()
-  let unique = {}
-  let newArray = []
+async function decrementCartHandler(e) {
+  decrementCart(e.target.getAttribute("data-id"));
+  location.reload();
+}
 
-  for (var i=0; i<jsonArray.length;i++){
-    let objId = jsonArray[i]["Id"]
-    unique[objId] = jsonArray[i]
-    if(!map.get(jsonArray[i].Id)) {
-      map.set(jsonArray[i].Id,1)
+const decrementCartItems = document.querySelectorAll(".decrement");
+
+decrementCartItems.forEach(function (item) {
+  item.addEventListener("click", decrementCartHandler);
+});
+
+function itemQuantity(data) {
+  var jsonArray = data;
+  var map = new Map();
+  let unique = {};
+  let newArray = [];
+
+  for (var i = 0; i < jsonArray.length; i++) {
+    let objId = jsonArray[i]["Id"];
+    unique[objId] = jsonArray[i];
+    if (!map.get(jsonArray[i].Id)) {
+      map.set(jsonArray[i].Id, 1);
     } else {
-      map.set(jsonArray[i].Id, map.get(jsonArray[i].Id) + 1)
+      map.set(jsonArray[i].Id, map.get(jsonArray[i].Id) + 1);
     }
   }
 
   for (i in unique) {
-    newArray.push(unique[i])
+    newArray.push(unique[i]);
   }
 
-  return [map, newArray]
+  return [map, newArray];
 }
