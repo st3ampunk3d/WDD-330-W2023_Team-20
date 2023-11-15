9ce851144277bd44c6aea3ffadf3e3c89f088cc1
mr.beale <j.b.beale2015@gmail.com>
2023-11-15 13:38:30 -0700

    Added product sorting

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -25,12 +25,45 @@ export default class ProductList {
   }
   async init() {
     const list = await this.dataSource.getData(this.category);
+
+    document
+    .getElementById("sort")
+    .addEventListener("change", this.sortOrder.bind(this));
+
     this.renderList(list);
+  }
+
+  async sortOrder() {
+    let list = await this.dataSource.getData(this.category);
+
+    var e = document.getElementById("sort");
+    var option = e.options[e.selectedIndex].text;
+    console.log(option)
+    list.sort((a,b) => {
+      let fa = a.NameWithoutBrand.toLowerCase()
+      let fb = b.NameWithoutBrand.toLowerCase()
+    
+      if (option == "Price") {
+        console.log("sorting by price")
+         return Number(a.FinalPrice) - Number(b.FinalPrice)
+      }
+      if (option == "Brand") {
+        console.log("sorting by Brand")
+        fa = a.Brand.Name.toLowerCase()
+        fb = b.Brand.Name.toLowerCase()
+      }
+      if (option == "Name") {
+        console.log("default sort")
+      }
+    
+      return fa.localeCompare(fb)
+    })
+    document.querySelector(".product-list").innerHTML = ""
+    this.renderList(list)
   }
 
   renderList(list) {
     renderListWithTemplates(productCardTemplate, this.listElement, list);
     document.querySelector(".title").innerHTML = this.category;
   }
-
 }
